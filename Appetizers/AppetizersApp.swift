7802884b0b1a72952bb8e23b5ab2b34829df8b70
i9orila <i.gorrila@icloud.com>
2023-02-07 12:27:10 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by I9orila on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
